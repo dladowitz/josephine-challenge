bddb55db1ac4cb9d9211acaaa9a4b40360c11b95
David Ladowitz <david@ladowitz.com>
2016-07-17 02:10:51 -0700

    fix logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   layout "landing_page"
-  
+
   def index
   end
 
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to '/'
   end
 end
